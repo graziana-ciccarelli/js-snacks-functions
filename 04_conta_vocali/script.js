c9aa9c9e3bda4991248dd083482bca56c43ad305
graziana-ciccarelli <grazianaciccarelli99@gmail.com>
2025-01-16 17:12:12 +0100

    fine esercizio (conta vocali)

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,12 +1,25 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
+
 
 
 // Dichiara la funzione qui.
+function contaVocali(stringa) {
+    let conteggio = 0;
+    const vocali = 'aeiou';
+    for (let i = 0; i < stringa.length; i++) {
+        if (vocali.includes(stringa[i].toLowerCase())) {
+            conteggio++;
+        }
+    }
+    return conteggio; 
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
+const word = 'javascript';
+const risultato = contaVocali(word);
+console.log(risultato);
 
 
 
