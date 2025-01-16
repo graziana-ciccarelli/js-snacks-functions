73a5e7b1949c21c51144e9ea61e061ed70ec30a7
graziana-ciccarelli <grazianaciccarelli99@gmail.com>
2025-01-16 15:56:29 +0100

    fine esercizio (iniziali)

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,16 @@
 
 
 // Dichiara la funzione qui.
-
-
+function lettereIniziali(){
+    let iniziali = [];
+    for (let i = 0; i < names.length; i++){
+      iniziali.push(names[i][0]); 
+    }
+    return iniziali;
+  }
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+console.log(lettereIniziali());
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
