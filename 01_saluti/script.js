f05d6451e3196d68d288a452c26e7323d43e135e
graziana-ciccarelli <grazianaciccarelli99@gmail.com>
2025-01-16 12:18:41 +0100

    fine esercizio(saluti)

---
+++ 01_saluti/script.js
@@ -0,0 +1,19 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+
+
+
+// Dichiara la funzione qui.
+function saluta(nome) {
+    return `Ciao ${nome}`;
+  }
+  
+
+
+// Invoca la funzione qui e stampa il risultato in console
+const userName = 'Mario';
+console.log(saluta(userName));
+
+
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
