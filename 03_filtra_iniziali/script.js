e4efc3f452076d20e10a0bf33ed39bdc8597ed62
graziana-ciccarelli <grazianaciccarelli99@gmail.com>
2025-01-16 15:13:47 +0100

    fine esercizio(filtra iniziali)

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,12 +1,24 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
-const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
+
 
 
 // Dichiara la funzione qui.
+function filtraPerLettera(namesArray, letter) {
+    let risultato = [];
+    for (let i = 0; i < namesArray.length; i++) {
+      if (namesArray[i][0] === letter) {
+        risultato.push(namesArray[i]);
+      }
+    }
+    return risultato;
+  }
 
 
 // Invoca la funzione qui e stampa il risultato in console
+
+const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
+console.log(filtraPerLettera(names, 'A'));
 
 
 
