e8ab14e93b2511a741b08a04c444cc330302c9ef
graziana-ciccarelli <grazianaciccarelli99@gmail.com>
2025-07-14 10:29:12 +0200

    corretto il passaggio dell'array di nomi nella funzione lettereIniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,19 +1,14 @@
-/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
-
-
-
+/* Scrivi una funzione che accetti un array di nomi e restituisca un nuovo array con le iniziali di ogni parola dell'array fornito */
 
 // Dichiara la funzione qui.
-function lettereIniziali(){
-    let iniziali = [];
-    for (let i = 0; i < names.length; i++){
-      iniziali.push(names[i][0]); 
-    }
-    return iniziali;
+function lettereIniziali(nomi) {
+  let iniziali = [];
+  for (let i = 0; i < nomi.length; i++) {
+    iniziali.push(nomi[i][0]); // aggiunge la prima lettera di ogni nome all'array iniziali
   }
+  return iniziali;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-console.log(lettereIniziali());
-
-//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+console.log(lettereIniziali(names)); // Risultato atteso: ["A", "L", "M", "A", "G", "A"]
