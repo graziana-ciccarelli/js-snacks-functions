b8ee2a230f9242b8856e108347c41f73346d3b0c
graziana-ciccarelli <grazianaciccarelli99@gmail.com>
2025-07-14 10:42:36 +0200

    add:ottimizzato la funzione filtraPerLettera utilizzando

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,25 +1,9 @@
-/* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
-
-
-
-
 // Dichiara la funzione qui.
 function filtraPerLettera(namesArray, letter) {
-    let risultato = [];
-    for (let i = 0; i < namesArray.length; i++) {
-      if (namesArray[i][0] === letter) {
-        risultato.push(namesArray[i]);
-      }
-    }
-    return risultato;
-  }
-
+  // filter per filtrare le parole che iniziano con la lettera specificata, ignorando maiuscole/minuscole
+  return namesArray.filter(name => name[0].toLowerCase() === letter.toLowerCase());
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-console.log(filtraPerLettera(names, 'A'));
-
-
-
-//Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+console.log(filtraPerLettera(names, 'A')); // ["Anna", "Adele", "Alessandra"]
